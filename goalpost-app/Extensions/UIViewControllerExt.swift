b60bf4c3795f6407343effc14cf1a6453923a14d
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-18 23:46:08 -0500

    Added extension to ViewController to behave as a navigation controller

---
+++ goalpost-app/Extensions/UIViewControllerExt.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewControllerExt.swift
+//  goalpost-app
+//
+//  Created by Ricardo Herrera Petit on 2/18/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        present(viewControllerToPresent, animated: false, completion: nil)
+    }
+    
+    func dismissDetail() {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        dismiss(animated: false, completion: nil)
+        
+    }
+}
