fe95f23723ef265b4d39ad2e248e986a2db4083d
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-18 19:13:46 -0500

    Setup GoalCell

---
+++ goalpost-app/Views/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Ricardo Herrera Petit on 2/18/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount:Int) {
+        self.goalProgressLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+        
+    }
+
+}
