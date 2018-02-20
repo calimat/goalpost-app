50229a9777b186e46158b6f01fae864afb2bb5bb
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-19 21:24:46 -0500

    CreatGoalVC interface builder and constraints

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateGoalVC: UIViewController {
+class CreateGoalVC: UIViewController, UITextViewDelegate {
     @IBOutlet weak var goalTextView: UITextView!
     @IBOutlet weak var shortTemBtn: UIButton!
     @IBOutlet weak var longTermBtn: UIButton!
@@ -21,10 +21,16 @@ class CreateGoalVC: UIViewController {
         nextBtn.bindToKeyboard()
         shortTemBtn.setSelectedColor()
         longTermBtn.setDeselectedColor()
+        goalTextView.delegate = self 
     
     }
 
     @IBAction func nextBtnWasPressed(_ sender: Any) {
+        if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else { return }
+            finishGoalVC.initData(description: goalTextView.text!, type: goalType)
+            presentDetail(finishGoalVC)
+        }
     }
     
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
@@ -42,5 +48,9 @@ class CreateGoalVC: UIViewController {
         dismissDetail()
     }
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.text = ""
+        goalTextView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
 
 }
