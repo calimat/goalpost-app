377931eaef99b7966f9e978e8e83ad5d4f8b15b3
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-19 20:41:36 -0500

    Bind button to keyboard and set button selected colors

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -14,17 +14,28 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType:GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.bindToKeyboard()
+        shortTemBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
+    
     }
 
     @IBAction func nextBtnWasPressed(_ sender: Any) {
     }
+    
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTemBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTemBtn.setDeselectedColor()
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
